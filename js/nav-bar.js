65fcc4b3236b1cf4c3ec25a52ee795998eb46c2b
João Vitor Moraes <joaovitorjuv@outlook.com>
2022-09-19 13:55:51 -0300

    alterado o disgn da página e adicionada a funcionalidade da nav bar

---
+++ js/nav-bar.js
@@ -0,0 +1,7 @@
+function openNav() {
+    document.getElementById("mySidenav").style.display = "block";
+  }
+  
+  function closeNav() {
+    document.getElementById("mySidenav").style.display = "none";
+  }
